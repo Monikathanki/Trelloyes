f16df72b0a936b5883a2dc13e45a7ffd232821f0
Monikathanki <thanki.monika2@gmail.com>
2020-09-19 20:18:24 -0700

    second commit

--- src/List.js
+++ src/List.js
@@ -13,13 +13,16 @@ export default function List(props) {
                 {props.cards.map((card) =>
                     <Card
                         key={card.id}
+                        id={card.id}
                         title={card.title}
                         content={card.content}
+                        onClickDelete={props.onClickDelete}
                     />
                 )}
                 <button
                     type='button'
                     className='List-add-button'
+                    onClick={() => props.onClickAdd(props.id)}
                 >
                     + Add Random Card
 
@@ -28,4 +31,8 @@ export default function List(props) {
 
         </section>
     )
+}
+
+List.defaultProps = {
+    onClickAdd: () => {},
 }
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App store={STORE}/>,
+    <App />,
   </React.StrictMode>,
   document.getElementById('root'),
 );
